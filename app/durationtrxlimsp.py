902afde3a8df220441448a68c0aa49d9dbe28442
Jimmyppq <141971415+Jimmyppq@users.noreply.github.com>
2024-06-18 19:35:42 -0500

    v5.0
    Se añade funcionalidad a duration para que cuando una transacción renga mas de un 1 send y un mas de un failover, se registren ambos, adicional se adiciona el campo inot en false para las transacciones que no tengan mas de 1 send y en true para aquellas transacciones con mas de un send

--- app/durationtrxlimsp.py
+++ app/durationtrxlimsp.py
@@ -44,49 +44,27 @@ def setup_logging():
 
     return logger, otro_logger, logger_write
 
-
 def compile_regular_expresion ():
     global pattern
     #compilar la expresión regular
-    # Regular expression for parsing the log line
-    #pattern = r"\[(?P<timestamp>.+?)\]\s+(?P<action>.+?)\s+(?P<subcomponent>.+?)\s+(?P<details>.+)"
-    #pattern = r"(?P<timestamp>\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2})\s+(?P<action>.+?)\s+(?P<subcomponent>.+?)\s+(?P<details>.+)"
-    ########################################################################################################################################################
-    ### here ####pattern = r"\[(?P<timestamp>\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}(?:\.\d{3})?)\]\s+(?P<action>.+?)\s+(?P<subcomponent>.+?)\s+(?P<details>.+)"
-    #[2021/12/15 23:12:16] TOREGISTER ServiceCenterOu transaction:ULr/+jE3vcAZNPFumuwl7s+X
-    #[2021/12/15 23:12:16] OUT        ServiceCenterOu transaction:ULr/+jE3vcAZNPFumuwl7s+X pri:4
-    ########################################################################################################################################################
-
-    #pattern = r"(?P<timestamp>\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}(?:\.\d{3})?)\s+(?P<action>.+?)\s+(?P<subcomponent>.+?)\s+(?P<details>.+)"
-    #2023/08/15 21:57:56 OUT        EventManager    event:UMbw23DMS0kIqe41BxllezcS
-    #2023/08/15 21:57:56 IN         WsSerInAdpAuth  transaction:UMbw23SQkLxRJu41Bxktkxuo
-    #2023/08/15 21:57:56 NEWTRANS   WsSerInAdpAuth  transaction:UMbw23SQkLxRJu41Bxktkxuo message:1692151076605
-    ########################################################################################################################################################
     pattern = re.compile(r"\[(?P<timestamp>\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}(?:\.\d{3})?)\]\s+(?P<action>.+?)\s+(?P<subcomponent>.+?)\s+(?P<details>.+)")
 
-
 def process_log_line(line):
-    
     """
     This function processes a log line and extracts relevant details.
     """
-
     # Regular expression for finding transaction ID and priority
-    #transaction_pattern = r"transaction:([^ ]*)"
     transaction_pattern = r"(transaction:)([^ ]*)"
-    #transaction_pattern = r"(transaction:|event:)([^ ]*)"
     priority_pattern = r"pri:(\d+)"
 
     # Parse the log line
     match = pattern.match(line)
-    ## here #### match = re.match(pattern, line)
 
     if match:
         # Extract details
         details = match.groupdict()
 
         # Find transaction ID if present
-        #transaction_match = re.search(transaction_pattern, details['details'])
         transaction_matches = re.finditer(transaction_pattern, details['details'])
         transaction_ids = []
         for transaction_match in transaction_matches:
@@ -111,16 +89,12 @@ def process_log_line(line):
         else:
             details['priority'] = -1
 
-
         if '.' in details['timestamp']:
             # Format with milliseconds
             details['timestamp'] = datetime.strptime(details['timestamp'], "%Y/%m/%d %H:%M:%S.%f")
         else:
             # Format without milliseconds
             details['timestamp'] = datetime.strptime(details['timestamp'], "%Y/%m/%d %H:%M:%S")
-
-        # Convert timestamp to datetime object
-        # details['timestamp'] = datetime.strptime(details['timestamp'], "%Y/%m/%d %H:%M:%S")
 
         return details
 
@@ -135,33 +109,16 @@ def log_file_generator(file_path):
                 yield processed_line
 
 def process_transactions(file_path):
-
-    # Inicialización del diccionario de transacciones con valores predeterminados
-    transactions = defaultdict(lambda: {
-        'date_min': None,
-        'date_max': None,
-        'priority': -1,  # por defecto -1 si no hay prioridad
-        'first_action': None,
-        'last_action': None,
-        'first_subcomponent': None,
-        'last_subcomponent': None,
-        'date_in_collector': None,
-        'mnewtrans': None,
-        'countSend': 0,
-        'Duration': None,
-        'duration_limsp': None
-    })
-
+    global global_transactions
     for detail in log_file_generator(file_path):
         transaction_id = detail['transaction_id']
         timestamp = detail['timestamp']
         action = detail['action']
         subcomponent = detail['subcomponent']
-        priority = detail.get('priority', -1)  # Usar -1 si la prioridad es None
+        priority = detail.get('priority', -1)
         mtransaction_id = detail.get('Mtransaction_id')
 
-        #verifica si la clave existe y, si no, la establece con el valor por defecto proporcionado
-        transaction = transactions.setdefault(transaction_id, {
+        transaction = global_transactions.setdefault(transaction_id, {
             'date_min': timestamp,
             'date_max': timestamp,
             'first_action': action,
@@ -169,9 +126,10 @@ def process_transactions(file_path):
             'first_subcomponent': subcomponent,
             'last_subcomponent': subcomponent,
             'priority': priority,
-            'date_in_collector': None,
+            'collector_times': [],
             'mnewtrans': mtransaction_id,
-            'countSend': 0
+            'countSend': 0,
+            'send_times': []
         })
 
         update_min = timestamp < transaction['date_min'] and transaction['first_action'] != 'NEWTRANS'
@@ -189,126 +147,140 @@ def process_transactions(file_path):
         
         transaction['countSend'] += action == 'SEND'
 
-        # Actualizar la prioridad, mnewtrans y contar las acciones SEND
+        if action == 'SEND':
+            transaction['send_times'].append(timestamp)
+
         if priority != -1:
             transaction['priority'] = priority
 
         if mtransaction_id is not None:
             transaction['mnewtrans'] = mtransaction_id
 
-        # Establecer 'date_in_collector' si las condiciones coinciden
         if action == 'OUT' and subcomponent == 'FailOverManager':
-            transaction['date_in_collector'] = timestamp
-    
-    cantidad_registros = len(transactions)
-    if (cantidad_registros > 0):
-        write_result (transactions)
+            transaction['collector_times'].append(timestamp)
+
+    cantidad_registros = len(global_transactions)
+    if cantidad_registros > 0:
+        logger_files.info(f"Processed {cantidad_registros} transactions from {file_path}")
     else:
-        logger_files.warning('no hay transacciones en este archivo')
+        logger_files.warning('No transactions in this file')
+
+def write_result():
+    records = []
+    for trans_id, data in global_transactions.items():
+        if not data['send_times'] and not data['collector_times']:
+            record = {
+                'Transaction ID': trans_id,
+                'date_min': data['date_min'],
+                'date_max': data['date_max'],
+                'priority': data['priority'],
+                'first_action': data['first_action'],
+                'last_action': data['last_action'],
+                'first_subcomponent': data['first_subcomponent'],
+                'last_subcomponent': data['last_subcomponent'],
+                'Duration': (data['date_max'] - data['date_min']).total_seconds(),
+                'mnewtrans': data['mnewtrans'],
+                'countSend': data['countSend'],
+                'date_in_collector': data.get('date_in_collector'),
+                'duration_limsp': None,
+                'node_name': node_name,
+                'inot': False
+            }
+            records.append(record)
         
-                 
-              
-def write_result(transactions):
-    # Convert the dictionary to a DataFrame
-    transactions_df = pd.DataFrame.from_dict(transactions, orient='index')    
-    transactions_df.index.name = 'Transaction ID'
+        send_times = data['send_times']
+        collector_times = data['collector_times']
+        
+        if len(send_times) == len(collector_times):
+            for i, (send_time, collector_time) in enumerate(zip(send_times, collector_times)):
+                inot_value = i > 0  # Marcar como True a partir del segundo SEND
+                record = {
+                    'Transaction ID': trans_id,
+                    'date_min': data['date_min'],
+                    'date_max': send_time,
+                    'priority': data['priority'],
+                    'first_action': data['first_action'],
+                    'last_action': 'SEND',
+                    'first_subcomponent': data['first_subcomponent'],
+                    'last_subcomponent': data['last_subcomponent'],
+                    'Duration': (send_time - data['date_min']).total_seconds(),
+                    'mnewtrans': data['mnewtrans'],
+                    'countSend': data['countSend'],
+                    'date_in_collector': collector_time,
+                    'duration_limsp': (collector_time - data['date_min']).total_seconds() if collector_time else None,
+                    'node_name': node_name,
+                    'inot': inot_value
+                }
+                records.append(record)
+        else:
+            logger_files.warning(f'Mismatch in the number of send_times and collector_times for transaction ID: {trans_id}')
 
-    # Verifica y convierte 'date_in_collector' a datetime, maneja la ausencia de la columna
-    #if 'date_in_collector' in transactions_df.columns:
+    transactions_df = pd.DataFrame(records)
 
-    transactions_df['date_in_collector'] = pd.to_datetime(transactions_df['date_in_collector'], errors='coerce')
-    transactions_df['Duration'] = (transactions_df['date_max'] - transactions_df['date_min']).dt.total_seconds()
-
-    # Calcula 'duration_limsp' donde 'date_in_collector' es válido
-    mask_valid_date = transactions_df['date_in_collector'].notnull()    
-    transactions_df['duration_limsp'] = float('nan')  # Inicializa con NaN
-    transactions_df.loc[mask_valid_date, 'duration_limsp'] = (transactions_df['date_in_collector'] - transactions_df['date_min']).dt.total_seconds()
-
-    # Replace NaN priorities with -1
-    if 'priority' in transactions_df.columns:
-        transactions_df['priority'] = transactions_df['priority'].fillna(-1)
-    else:
-        transactions_df['priority'] = -1
-
-    # Rearrange columns, asegurándose de que todas las columnas necesarias existan
-    columns_order = [
-        'date_min', 'date_max', 'priority', 'first_action', 'last_action',
-        'first_subcomponent', 'last_subcomponent', 'Duration', 'mnewtrans',
-        'countSend', 'date_in_collector', 'duration_limsp'
-    ]
-    transactions_df = transactions_df[columns_order]
-
-    transactions_df['node_name'] = node_name
-
-    # Append or write to the CSV file
-    write_mode = 'a' if countFiles > 0 else 'w'
-    header = countFiles == 0
-    transactions_df.to_csv(config['OutputFilePath'], mode=write_mode, header=header, index=True)
-    logger_write.info(f"CountFile: {countFiles}. Records: {transactions_df.shape[0]} -- FileName: {file_name}")
+    transactions_df.to_csv(config['OutputFilePath'], mode='w', header=True, index=False)
+    logger_write.info(f"Total Records: {transactions_df.shape[0]} -- FileName: {config['OutputFilePath']}")
 
 
 def process_log_files(directory_path, file_pattern):
-    """
-    This function processes all log files in a directory and its subdirectories that match a given pattern.
-    """
     global countFiles
     global file_name
     global node_name
     
     logger_principal.info('Starting processing log Files...')
-    # Get the directory path
     directory_path = Path(directory_path)
     
-    # Get a list of all files that match the patter
     matching_files = list(directory_path.rglob(file_pattern))
     
-    # Check if no files were found
     if not matching_files:
         logging.error('No files found. Terminating the script.')
-        exit(1)
     
     logger_principal.info(f'File search results: {len(matching_files)} files...')
-    # Iterate over each file in the directory and its subdirectories
     for file_path in directory_path.rglob(file_pattern):
-        # Process the log file
         file_path = Path(file_path)
-        node_name = file_path.parent.name  # Obtener sólo el último directorio
+        node_name = file_path.parent.name
         file_name = file_path.name
         path, file_name = os.path.split(file_path)
         logger_files.info(f'Nodo: {node_name} -- Archivo: {file_name} -- Path: {path}')        
         process_transactions(file_path)       
-        countFiles+=1
+        countFiles += 1
         if countFiles % 20 == 0:
             logger_principal.info(f'Ha terminado de procesar {countFiles} archivos...')
+    
+    # Verificar si el archivo existe y eliminarlo si es necesario
+    if os.path.exists(config['OutputFilePath']):
+        os.remove(config['OutputFilePath'])
+    
+    # Escribir el archivo CSV con todas las transacciones acumuladas
+    write_result()
 
 
-#main   
+global_transactions = defaultdict(lambda: {
+    'date_min': None,
+    'date_max': None,
+    'priority': -1,
+    'first_action': None,
+    'last_action': None,
+    'first_subcomponent': None,
+    'last_subcomponent': None,
+    'collector_times': [],
+    'mnewtrans': None,
+    'countSend': 0,
+    'send_times': [],
+    'Duration': None,
+    'duration_limsp': None
+})
+
+
+#main
 config = load_config('./config/config.ini')
 logger_principal, logger_files, logger_write = setup_logging()
 compile_regular_expresion()
 
-
-
 countFiles = 0
 
-# Process all log files in the given directory and its subdirectories that match the given pattern
-# Call the function with your directory path and file pattern
-#process_log_files("/mnt/NAS/BANORTE/259_LATSUP-2959_Monitoreo upselling 300TPS entorno Producción/2023-08-04 01 L01/F1", "*act*.log")
-#ofi
 process_log_files(config['InputPath'], config['FilePattern'] )
 
-#process_log_files("../data/", "escenario4*.log")
-#home
-#process_log_files("/Users/colombia-01/OneDrive - Latinia Interactive Business, S.A/Jimmy/brrMac/logsPrueba/", "logActDR.log")
-
-logger_principal.info('Processing completed...')
-
-logger_principal.info('Saving results...')
+logger_principal.info('Processing completed…')
+logger_principal.info('Saving results…')
 archivoResultante = config['OutputFilePath']
-# Save the DataFrame to a CSV file
-#ofi
-#all_transactions_df.to_csv("../output/result_prueba20112023_escenario4.csv")
-#home3
-#all_transactions_df.to_csv("/Users/colombia-01/Library/CloudStorage/OneDrive-LatiniaInteractiveBusiness,S.A/Jimmy/utiles/Python/piase/output/result_prueba04122023_1.csv")
-#all_transactions_df.to_csv("/home/scriptPython/Ver_2/piase/output/result_Banorte_02102023_F1.csv")
 logger_principal.info(f'Result stored {archivoResultante}')
